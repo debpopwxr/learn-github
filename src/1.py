5c9d0537430f3f4e633fb9361a228477c0482016
debpopwxr <debpopwxr@gmx.com>
2025-03-03 13:46:06 +0000

    Add 1.py

---
+++ src/1.py
@@ -0,0 +1,9 @@
+def random_python_code():
+    # Generate a random number between 1 and 10
+    rand = random.randint(1, 10)
+    
+    # Check if the number is even or odd
+    if rand % 2 == 0:
+        print("The number", rand, "is even")
+    else:
+        print("The number", rand, "is odd")
